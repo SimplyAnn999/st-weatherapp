3799aff7b231294770ec493c4afe2d703ea70234
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-27 18:35:17 -0500

    streamlit_app.py
    Codes for application

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,14 @@ import requests
 import streamlit as st
 
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
+
+# WEB HEADER SECTION
+st.title("A Minimalist Weather App")
 city = st.text_input("Enter your city : ")
+st.write('Temperature: {}  degree celsius'.format(temp))
+st.write('wind_speed: {}  m/s' .format(wind_speed))
+st.write('Humidity: {} %' .format(humidity))
+st.write('Clouds:', description)
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
 
