860d21bbbc422df7f133fb37cb82e7184dbe6a8e
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-27 22:04:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,6 @@
 import requests
 import streamlit as st
 
-# ----API Call/Config
-url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
-
-res = requests.get(url)
-data = res.json()
-
-temp = data['main']['temp']
-wind_speed = data['wind']['speed']
-humidity = data['main']['humidity']
-description = data['weather'][0]['description']
-
 # ----Web/Tab config
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
 
@@ -29,3 +18,14 @@ if temp >= 149:
   st.write("You should wear a coat.")
 else:
   st.write("It is safe to wear shorts.")
+
+# ----API Call/Config
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
+
+res = requests.get(url)
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+humidity = data['main']['humidity']
+description = data['weather'][0]['description']
