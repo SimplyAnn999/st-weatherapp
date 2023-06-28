6062a0db0f030039ef9e92612d5a5a1ae0009de4
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-27 20:33:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548
 res = requests.get(url)
 data = res.json()
 
-st.down ('temp = data['main']['temp']')
+#temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 humidity = data['main']['humidity']
 description = data['weather'][0]['description']
