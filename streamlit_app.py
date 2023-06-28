4cd5095aaf340dd548bb87e9146d33e74852e2e7
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-27 20:25:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,15 @@
 import requests
 import streamlit as st
+import warnings
 
 # ----Web/Tab config
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
-hide_error = """
-<style>
-class="stException" {
-  visibility:hidden;
-}
-</style>
-"""
+warnings.filterwarnings('ignore')
 
 # ----WEB HEADER SECTION 1
 st.title("The Minimalist Weather App")
 city = st.text_input("Enter your city : ")
-st.markdown(hide_error,unsafe_allow_html=True)
+
 
 # ----API Call/Config
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
