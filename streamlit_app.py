7e662c07da254392b4a4c9a9ab2d570c6a6df36a
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-06-27 21:21:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ if 'key' not in st.session_state:
 
 # ----WEB HEADER SECTION 1
 st.title("The Minimalist Weather App")
-city = st.text_input("Enter your city : ")
+st.text_input("Enter your city : ")
 
 # ----API Call/Config
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
@@ -18,20 +18,12 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548
 res = requests.get(url)
 data = res.json()
 
-st.session_state['temp'] = data['main']['temp']
+temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 humidity = data['main']['humidity']
 description = data['weather'][0]['description']
 
-
 # ----WEB HEADER SECTION 2
-st.write('Temperature: {}  degree celsius'.format(temp))
-st.write('wind_speed: {}  m/s' .format(wind_speed))
-st.write('Humidity: {} %' .format(humidity))
-st.write('Clouds:', description)
-
-# ----HEADER SECTION
-st.title("A Minimalist Weather App")
 st.write('Temperature: {}  degree celsius'.format(temp))
 st.write('wind_speed: {}  m/s' .format(wind_speed))
 st.write('Humidity: {} %' .format(humidity))
