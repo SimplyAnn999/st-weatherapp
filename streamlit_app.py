e3e171a4efb6d88dd61d1d46e5d689b4fe0ecbff
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-07-04 14:30:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,6 @@ import streamlit as st
 # ----Web/Tab config
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
 
-# ----WEB HEADER SECTION 1
-st.title("The Minimalist Weather App")
-city = st.text_input("Enter your city : ")
-
-# ----WEB HEADER SECTION 2
-st.write('Temperature: {}  degree celsius'.format(temp))
-st.write('wind_speed: {}  m/s' .format(wind_speed))
-st.write('Humidity: {} %' .format(humidity))
-st.write('Clouds:', description)
-
-if temp >= 149:
-  st.write("You should wear a coat.")
-else:
-  st.write("It is safe to wear shorts.")
-
 # ----API Call/Config
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
 
@@ -29,3 +14,19 @@ temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 humidity = data['main']['humidity']
 description = data['weather'][0]['description']
+
+# ----WEB HEADER SECTION 1
+st.title("The Minimalist Weather App")
+city = st.text_input("Enter your city : ")
+
+# ----WEB HEADER SECTION 2
+st.note('Temperature: {}  degree celsius'.format(temp))
+st.note('wind_speed: {}  m/s' .format(wind_speed))
+st.note('Humidity: {} %' .format(humidity))
+st.note('Clouds:', description)
+
+if temp >= 149:
+  st.write("You should wear a coat.")
+else:
+  st.write("It is safe to wear shorts.")
+
