a4e31dabb4813c2b5fac1a23317e5b5c85ba4c53
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-08-08 14:10:42 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import requests
 import streamlit as st
 
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
+
 city = input("Enter your city : ")
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
@@ -15,11 +16,16 @@ humidity = data['main']['humidity']
 clouds = data['clouds']['all']
 description = data['weather'][0]['description']
 
+
+# HEADER SECTION
+st.subheader ("WeatherApp :sun:")
 st.title("A Minimalist Weather App")
 st.write('Temperature: {}  degree celsius'.format(temp))
 st.write('wind_speed: {}  m/s' .format(wind_speed))
 st.write('Humidity: {} %' .format(humidity))
-st.write('Clouds:', description)
+st.write('Clouds:', clouds)
+st.write('Description:', description)
+
 
 if temp >= 149:
   st.write("You should wear a coat.")
