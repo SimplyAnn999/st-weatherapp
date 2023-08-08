33bbdf3d128c1c391fe84fbd633d191ecce719f6
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-08-08 17:02:07 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,41 @@
 import requests
 import streamlit as st
 
+# ----Web/Tab config
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
-st.title("A Minimalist Weather App")
 
-st.write = input("Enter your city : ")
+if 'key' not in st.session_state:
+  st.session_state['key'] = 'value'
+  
+
+# ----WEB HEADER SECTION 1
+st.title("The Minimalist Weather App")
+city = st.text_input("Enter your city : ")
+
+# ----API Call/Config
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
+
 res = requests.get(url)
 data = res.json()
 
-temp = data['main']['temp']
+st.session_state['temp'] = data['main']['temp']
 wind_speed = data['wind']['speed']
 humidity = data['main']['humidity']
-clouds = data['clouds']['all']
 description = data['weather'][0]['description']
 
 
-# HEADER SECTION
-st.subheader ("WeatherApp :sun:")
+# ----WEB HEADER SECTION 2
+st.write('Temperature: {}  degree celsius'.format(temp))
+st.write('wind_speed: {}  m/s' .format(wind_speed))
+st.write('Humidity: {} %' .format(humidity))
+st.write('Clouds:', description)
+
+# ----HEADER SECTION
 st.title("A Minimalist Weather App")
 st.write('Temperature: {}  degree celsius'.format(temp))
 st.write('wind_speed: {}  m/s' .format(wind_speed))
 st.write('Humidity: {} %' .format(humidity))
-st.write('Clouds:', clouds)
-st.write('Description:', description)
-
+st.write('Clouds:', description)
 
 if temp >= 149:
   st.write("You should wear a coat.")
