b127e4a752a81803ee51d1d04ed38621b0b98b92
SimplyAnn999 <119898569+SimplyAnn999@users.noreply.github.com>
2023-08-08 17:10:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,18 @@ import streamlit as st
 
 # ----Web/Tab config
 st.set_page_config(page_title="WeatherApp", page_icon=":sun_behind_rain_cloud:", layout="wide")
-
-if 'key' not in st.session_state:
-  st.session_state['key'] = 'value'
-  
+hide_error = """
+<style>
+#class="stException" {
+  visibility:hidden;
+}
+</style>
+"""
 
 # ----WEB HEADER SECTION 1
 st.title("The Minimalist Weather App")
 city = st.text_input("Enter your city : ")
+st.markdown(hide_error,unsafe_allow_html=True)
 
 # ----API Call/Config
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548754ca009222d24da49'.format(city)
@@ -18,11 +22,10 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=c8efac34cc3548
 res = requests.get(url)
 data = res.json()
 
-st.session_state['temp'] = data['main']['temp']
+temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 humidity = data['main']['humidity']
 description = data['weather'][0]['description']
-
 
 # ----WEB HEADER SECTION 2
 st.write('Temperature: {}  degree celsius'.format(temp))
